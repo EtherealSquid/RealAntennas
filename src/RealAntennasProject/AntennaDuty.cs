e416664acdabc976d219e88516537bc5ce6cc902
EtherealSquid <Charli.Selene.Email@gmail.com>
2026-08-12 22:25:26 +1000

    Antenna duty, direction, transcievers
    Add antenna duty/direction, dual-band transceivers, relay-aware science routing

---
+++ src/RealAntennasProject/AntennaDuty.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RealAntennas
+{
+    // What kind of traffic an antenna/link is permitted to carry.
+    // Telemetry for command & Control and Science for Science data
+    [Flags]
+    public enum AntennaDuty
+    {
+        Telemetry = 1 << 0,
+        Science = 1 << 1,
+        Both = Telemetry | Science
+    }
+
+    public static class AntennaDutyExtensions
+    {
+        public const string DefaultModeName = "Both";
+        public static readonly string[] ModeNames = { "Telemetry", "Science", DefaultModeName };
+        public static readonly string[] ModeDisplayNames = { "Telemetry Only", "Science Only", "Telemetry + Science" };
+
+        public static AntennaDuty FromModeName(string name) => name switch
+        {
+            "Telemetry" => AntennaDuty.Telemetry,
+            "Science" => AntennaDuty.Science,
+            _ => AntennaDuty.Both,
+        };
+
+        public static string ToModeName(this AntennaDuty duty) => duty switch
+        {
+            AntennaDuty.Telemetry => "Telemetry",
+            AntennaDuty.Science => "Science",
+            _ => "Both",
+        };
+
+        public static bool CanHandleTelemetry(this AntennaDuty duty) => (duty & AntennaDuty.Telemetry) != 0;
+        public static bool CanHandleScience(this AntennaDuty duty) => (duty & AntennaDuty.Science) != 0;
+    }
+}
---
+++ src/RealAntennasProject/AntennaRFDirection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RealAntennas
+{
+    
+    // The physical RF direction(s) a transceiver is capable of. Named in
+    // hardware terms (Transmit/Receive) since "uplink"/"downlink" depends on reference
+    [Flags]
+    public enum AntennaRFDirection
+    {
+        Transmit = 1 << 0,
+        Receive = 1 << 1,
+        Bidirectional = Transmit | Receive
+    }
+
+    public static class AntennaRFDirectionExtensions
+    {
+        public const string DefaultModeName = "Bidirectional";
+        // Order matters: this is also the UI_ChooseOption cycle order in the VAB.
+        public static readonly string[] ModeNames = { "Uplink", "Downlink", DefaultModeName };
+        public static readonly string[] ModeDisplayNames = { "Uplink Only (Receive)", "Downlink Only (Transmit)", "Bidirectional" };
+
+        public static AntennaRFDirection FromModeName(string name) => name switch
+        {
+            "Uplink" => AntennaRFDirection.Receive,
+            "Downlink" => AntennaRFDirection.Transmit,
+            _ => AntennaRFDirection.Bidirectional,
+        };
+
+        public static bool CanTransmitRF(this AntennaRFDirection dir) => (dir & AntennaRFDirection.Transmit) != 0;
+        public static bool CanReceiveRF(this AntennaRFDirection dir) => (dir & AntennaRFDirection.Receive) != 0;
+    }
+}
